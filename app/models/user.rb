d69f4dedbb782068fd7f65b86809f7f452248545
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2023-03-25 20:34:23 -0300

    Configured model user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:cpf]
+
+  validates_uniqueness_of :cpf
+  validates_presence_of :name, :cpf, :email
+  
+  VISUALIZADOR = 'Visualizador'
+  SUPERVISOR = 'Supervisor'
+  COORDENADOR = 'Coordenador'
+  GESTOR_TIC = 'TIC'
+
+  PROFILE = [
+    VISUALIZADOR,
+    SUPERVISOR,
+    COORDENADOR,
+    TIC
+  ]
+       
 end
