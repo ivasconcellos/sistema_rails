7d60e55bf87612ea4aa30c93cf7eb04adcb0e864
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2023-03-25 22:56:57 -0300

    Configured registration edit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up) do |user_params|        
             user_params.permit(:name, :email, :cpf, :profile, :password, :password_confirmation, :current_password)    
         end  
+        devise_parameter_sanitizer.permit(:account_update) do |user_params|        
+            user_params.permit(:name, :email, :cpf, :profile, :password, :password_confirmation, :current_password)    
+        end  
+
     end
 
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, except: :about
 
   def initial
+    @user = User.find(current_user.id)
   end
 
   def about
