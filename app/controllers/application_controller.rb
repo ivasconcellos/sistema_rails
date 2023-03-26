6366f21b5ce7486971e7db371aaf684d18242e4f
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2023-03-25 22:02:46 -0300

    Config registration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
 	    end
     end
 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
+    def configure_permitted_parameters     
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|        
+            user_params.permit(:name, :email, :cpf, :profile, :password, :password_confirmation, :current_password)    
+        end  
+    end
+
+
 end
