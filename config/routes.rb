0d68ff12bd82435b582c07d25cfad53075eb25a2
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2023-03-25 23:18:36 -0300

    Configured admin.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
 
 
-  devise_for :admins
+  
   devise_for :users
+  devise_for :admins, :skip => [:registrations]
   
   #Initial Page
   as :user do
     root :to => "devise/sessions#new"
   end
+  
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
